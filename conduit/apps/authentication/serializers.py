20c02359a48e9e90e68fd4a0ec54c31894b223b9
Atul Lal <atul.lal123@gmail.com>
2019-06-19 23:14:04 +0530

    Barebone Authentication for any project

--- conduit/apps/authentication/serializers.py
+++ conduit/apps/authentication/serializers.py
@@ -59,5 +59,29 @@ class LoginSerializer(serializers.Serializer):
             'token': user.token
         }
 
+class UserSerializer(serializers.ModelSerializer):
 
+    password = serializers.CharField(
+        max_length = 128,
+        min_length = 8,
+        write_only = True
+    )
 
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password', 'token')
+
+        read_only_field = ('token', )
+    
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+
+        for (key, value) in validated_data.items():
+            setattr(instance, key, value)
+        
+        if password is not None:
+            instance.set_password(password)
+        
+        instance.save()
+
+        return instance
