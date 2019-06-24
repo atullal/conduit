ccb9cd1640dc4ca8da260075c6dc74d7c7abace0
Atul Lal <atul.lal123@gmail.com>
2019-06-24 23:22:55 +0530

    Postgresql Database instead of SQLite

--- conduit/settings.py
+++ conduit/settings.py
@@ -82,12 +82,24 @@ WSGI_APPLICATION = 'conduit.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
+    "default": {
+
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+
+        "NAME": "postgres",
+
+        "USER": "postgres",
+
+        "PASSWORD": "<PASSWORD>",
+
+        "HOST": "localhost",
+
+        "PORT": "5433",
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
