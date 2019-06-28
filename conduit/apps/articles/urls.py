bc965dca4f1b1db78f30f93b20f0af7ff2461702
Atul Lal <atul.lal123@gmail.com>
2019-06-29 01:48:23 +0530

    Added Comment API url

--- conduit/apps/articles/urls.py
+++ conduit/apps/articles/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
-from .views import ArticleViewSet
+from .views import ArticleViewSet, CommentsListCreateAPIView
 
 
 router = DefaultRouter(trailing_slash=False)
@@ -9,5 +9,10 @@ router.register(r'articles', ArticleViewSet)
 
 app_name = 'articles'
 urlpatterns = [
+    
     path('', include(router.urls)),
+    path(
+        'articles/<slug:article_slug>/comments',
+        CommentsListCreateAPIView.as_view()
+    ),
 ] 
